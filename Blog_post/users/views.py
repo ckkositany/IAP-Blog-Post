d6e7d258878013175d2ca91d69f9abacc23e17d5
Kositanyck <kositanyck@gmail.com>
2024-04-04 15:30:31 +0300

    Allow users to update their profile info from profile page

--- Blog_post/users/views.py
+++ Blog_post/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -21,7 +21,27 @@ def register(request):
 #creating the user's profile page
 @login_required #Adds functionality to the profile view
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance= request.user)
+        p_form = ProfileUpdateForm(request.POST, 
+                                   request.FILES, 
+                                   instance= request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile')
+       
+    else:
+        u_form = UserUpdateForm(instance= request.user)
+        p_form = ProfileUpdateForm(instance= request.user.profile)
+         
+    context = {
+                'u_form': u_form,
+                'p_form': p_form,
+               
+               }
+    return render(request, 'users/profile.html', context)
 
 
 
