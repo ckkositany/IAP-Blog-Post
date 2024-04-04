d16d8f057297b503b71026b587ba2c70b486f833
Kositanyck <kositanyck@gmail.com>
2024-04-04 12:12:04 +0300

    serving files uploaded by user

--- Blog_post/Blog_post/urls.py
+++ Blog_post/Blog_post/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,7 @@ urlpatterns = [
     path('', include('Blog.urls')),
 
 ]
+
+#serving files uploaded by user during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
