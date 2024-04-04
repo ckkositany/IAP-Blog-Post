5ad3f703ee4e42491344eb1fc4c1ce84916b48c7
Kositanyck <kositanyck@gmail.com>
2024-04-04 19:45:17 +0300

    ListView to view existing posts on home page

--- Blog_post/Blog/urls.py
+++ Blog_post/Blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Blog-home'),
+    path('', PostListView.as_view(), name='Blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # handles specific blog as per its id
     path('about/', views.about, name='Blog-about'),
 ]
--- Blog_post/Blog/views.py
+++ Blog_post/Blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'Blog/home.html', context)
+
+
+#creating a list of post
+class PostListView(ListView):
+    model = Post
+    template_name = 'Blog/home.html' #<app>/<model>_<view_type>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] #change the order to display the latest blogs
+
+
+#Handles detailview of posts
+class PostDetailView(DetailView):
+    model = Post
+   
+
 def about(request):
     return render(request, 'Blog/About.html')
