f8e72ecc12a9af03526ca7eb26d8852fb07b70c0
Kositanyck <kositanyck@gmail.com>
2024-04-04 13:24:49 +0300

    Handling new user profile page to add default image-profile

--- Blog_post/users/apps.py
+++ Blog_post/users/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+
+    #importing users signals from signals.py inside the ready function here
+    def ready(self):
+        import users.signals
