ebdb0f1d41012fca3510f7feee94c34ca34dfd59
Kositanyck <kositanyck@gmail.com>
2024-04-13 21:59:58 +0300

    paginating a single user posts

--- Blog_post/Blog/urls.py
+++ Blog_post/Blog/urls.py
@@ -4,11 +4,13 @@ from .views import (PostListView,
                       PostCreateView,
                       PostUpdateView,
                       PostDeleteView,
+                      UserPostListView,
                       )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='Blog-home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # handles specific blog as per its id
     path('post/new/', PostCreateView.as_view(), name='post-create'), # handles new created blog
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'), # handles specific blog as per its id
--- Blog_post/Blog/views.py
+++ Blog_post/Blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from django.views.generic import (ListView,
@@ -23,6 +24,20 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted'] #change the order to display the latest blogs
     paginate_by = 5
+
+
+#Handles pagination of a single user posts
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'Blog/user_posts.html' #<app>/<model>_<view_type>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted') #change the order to display the latest blogs
+
 
 
 #Handles detailview of posts
