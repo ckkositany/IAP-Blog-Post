c8790365c76f23c7768cbc7efe32aab81ef2c59b
Kositanyck <kositanyck@gmail.com>
2024-04-13 15:43:34 +0300

    Adding pagination on No. of posts in a page

--- Blog_post/Blog/views.py
+++ Blog_post/Blog/views.py
@@ -22,6 +22,7 @@ class PostListView(ListView):
     template_name = 'Blog/home.html' #<app>/<model>_<view_type>.html
     context_object_name = 'posts'
     ordering = ['-date_posted'] #change the order to display the latest blogs
+    paginate_by = 5
 
 
 #Handles detailview of posts
